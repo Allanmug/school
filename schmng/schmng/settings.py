05c9388ba6d64b01ec962281b3c5e4ca07c86cff
mugisha <mugishaeng@gmail.com>
2024-07-19 21:04:30 +0300

    static correction

--- schmng/schmng/settings.py
+++ schmng/schmng/settings.py
@@ -116,29 +116,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+ 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-# WhiteNoise settings for efficient static file serving
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# Enable versioning by appending file hashes to static file URLs
-WHITENOISE_KEEP_ONLY_HASHED_FILES = True
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 # Authentication settings
 AUTH_USER_MODEL = 'core.User'
 LOGIN_REDIRECT_URL = '/dashboard/'
